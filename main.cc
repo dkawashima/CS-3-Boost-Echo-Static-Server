8a6220b7ac7a5369e456434553800595bca2c5b0
Christopherhaack <christophermoorehaack@gmail.com>
2016-04-15 18:17:45 -0700

    changed main.cc and example_config

--- main.cc
+++ main.cc
@@ -12,7 +12,7 @@ static int getPort(const NginxConfig &config) {
       if (!kl) {
         try { return stoi(token); } catch (...) {}
       }
-      kl = (token != "port");
+      kl = (token != "listen");
     }
   }
   return -1;
@@ -20,30 +20,40 @@ static int getPort(const NginxConfig &config) {
 
 int main(int argc, char* argv[])
 {
-	using namespace std; // For atoi.
-  try
+	using namespace std; 
+	try
   {
     if (argc != 2)
     {
-      std::cerr << "Usage: blocking_tcp_echo_server <port>\n";
+      std::cerr << "Usage: webserver <config_file>\n";
       return 1;
     }
     /* copied from yichi server.main lines 17 to 21 */
     NginxConfigParser config_parser;
     NginxConfig config;
     if (!config_parser.Parse(argv[1], &config)) {
-    return -1;
+    	return -1;
     }
-    boost::asio::io_service io_service;
+   	boost::asio::io_service io_service;
     int port_ = getPort(config);
-   	
+   	std::cout << port_ << "\n";
     boost::asio::io_service io;
     tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), port_));
+	std::string reply = "HTTP/1.1 200 OK\r\n Content-Type: text/html\r\n\r\n <html><body>Hello, world!</body></html>\r\n";
     while (true){
+		boost::system::error_code read_error;
         tcp::socket socket(io);
         acceptor.accept(socket);
-        std::string reply = "HTTP/1.0 200 <html> Content-Type: text/html\n <body>\nHello, world!\n</body>\n</html>";
-        boost::asio::write(socket, boost::asio::buffer(reply));
+		boost::asio::streambuf buffer; 
+		boost::asio::read_until(socket, buffer, "\n", read_error);
+		std::istream str(&buffer);
+		std::string request;
+		std::getline(str, request);
+		if (request.find("GET") == 0){
+			boost::asio::write(socket, boost::asio::buffer(reply));
+		}
+		std::cout << "Got here!, Port: " << port_;
+		
 	}
   }
   catch (std::exception& e)
