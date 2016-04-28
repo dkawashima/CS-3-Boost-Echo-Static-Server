4788987024e6c543aefb7a84c2957b637a09a78a
user <user@ubuntu.(none)>
2016-04-28 02:26:26 -0400

    added mime_type functionality, also reduce number of reply cases so that our code is easier to look at

--- handlers.h
+++ handlers.h
@@ -83,6 +83,12 @@ private:
   /// invalid.
   static bool url_decode(const std::string& in, std::string& out);
 };
+namespace mime_types {
+
+/// Convert a file extension into a MIME type.
+std::string extension_to_type(const std::string& extension);
+
+} // namespace mime_types
 
 } // namespace server
 } // namespace http
